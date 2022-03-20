07bb0e2af75b94a4a5418da325e11646cf05ecb8
Yimang-Github <47217661+Yimang-Github@users.noreply.github.com>
2022-03-20 20:53:25 +0800

    slight modification

--- R/jojo_palette.R
+++ R/jojo_palette.R
@@ -24,19 +24,19 @@ jojo_palette <- list(
 
 jojo_stand <- function(name, n, type = c("discrete", "continuous")) {
   type <- match.arg(type)
-  
+
   pal <- jojo_palette[[name]]
   if (is.null(pal))
     stop("Palette not found.")
-  
+
   if (missing(n)) {
     n <- length(pal)
   }
-  
+
   if (type == "discrete" && n > length(pal)) {
     stop("Number of requested colors greater than what palette can offer")
   }
-  
+
   out <- switch(type,
                 continuous = grDevices::colorRampPalette(pal)(n),
                 discrete = pal[1:n]
@@ -44,17 +44,3 @@ jojo_stand <- function(name, n, type = c("discrete", "continuous")) {
   structure(out, class = "palette", name = name)
 }
 
-#' @export
-#' @importFrom graphics rect par image text
-#' @importFrom grDevices rgb
-print.stand <- function(x, ...) {
-  n <- length(x)
-  old <- par(mar = c(0.5, 0.5, 0.5, 0.5))
-  on.exit(par(old))
-  
-  image(1:n, 1, as.matrix(1:n), col = x,
-        ylab = "", xaxt = "n", yaxt = "n", bty = "n")
-  
-  rect(0, 0.9, n + 1, 1.1, col = rgb(1, 1, 1, 0.8), border = NA)
-  text((n + 1) / 2, 1, labels = attr(x, "name"), cex = 1, family = "serif")
-}
