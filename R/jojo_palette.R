f9ac2d0dfeca284dec4451242560842ad178d2a3
Yimang-Github <47217661+Yimang-Github@users.noreply.github.com>
2022-09-13 22:05:13 +0800

    add new palettes

--- R/jojo_palette.R
+++ R/jojo_palette.R
@@ -1,9 +1,11 @@
 jojo_palette <- list(
+  #season names
   PhantonBlood = c("#7b3fb0","#4d3b9f","#617bc1","#a34838","#e3be4c"), #https://www.color-hex.com/color-palette/79970
   BattleTendency = c("#5e4734","#854545","#4e6746","#c7b050","4e6c87"), #https://www.color-hex.com/color-palette/79971
   StardustCrusaders= c("#b25942","#e3cb67","#476b43","#323630","#c5c5c5"), #https://www.color-hex.com/color-palette/79972
   DiamondUnbreakable = c("#480f7c","#6d4190","#fad30a","#c73f75","#b71154"), #https://www.schemecolor.com/diamond-is-unbreakable.php
   StoneOcean = c("#ecc88c","#50bd47","#dfc356","#d1a045","#373749"), #https://www.color-hex.com/color-palette/109687
+  #characters
   Jonathan = c("#060329","#1e12d0","#1470e8","#7d12d0","593112"), #https://www.color-hex.com/color-palette/39076
   Joseph = c("#ffe735","#70bd14","#9041cc","#632c8d","#417ecc"), #https://www.color-hex.com/color-palette/39078
   Jotaro1 = c("#e5ddff","#3f78A3","#805acc","#c234e3","#33435d"), #https://www.pinterest.com/pin/709879960009812954/
@@ -11,15 +13,19 @@ jojo_palette <- list(
   Dio = c("#50ecea","#00c0c0","#283bf7","#d1cea7","	#ffd801"), #https://www.color-hex.com/color-palette/96066
   Josuke = c("#243182","#eeb737","#fa5d55","#922956","#a86585"), #https://www.colourlovers.com/palette/4516087/Josuke_Higashikata
   Rohan = c("#dcb5b0","#c777b6","#f7ef95","#97db8c","#263231"), #https://jojo.fandom.com/f/p/4400000000000092610
-  Giovana = c("#ffd666","#ffe2ba","#d66cff","#3c53ff","#3d3396"), #https://www.colourlovers.com/palette/4710946/Giorno_Giovanna?widths=1
+  Kira = c("#a4a1d6","#b3dda3","#e6e480","#e091c3","#d0ad61"),
   Koichi = c("#225544","#9d9f9d","#49305e","#d99233","#98b2e9"),
-  Jolyne = c("#f272ca","#a2b711","#44b2b1","#0f6496","#141e39"), #https://jojo.fandom.com/f/p/4400000000000092610
+  Giovana = c("#ffd666","#ffe2ba","#d66cff","#3c53ff","#3d3396"), #https://www.colourlovers.com/palette/4710946/Giorno_Giovanna?widths=1
   Fugo = c("#2e3e8c","#f7b25c","#aabe57","#b43b7f","#5e3c9b"), #https://jojo.fandom.com/f/p/4400000000000092610
   Trish = c("#5a487a","#2d243d","#fbd125","#dd458c","#eyyfae"), #https://www.color-hex.com/color-palette/39081
+  Mista = c("#913a31","#1b5b9a","#f8a877","#6d57a7","#e6ba65"),
   Diavolo = c("#765db5","#409c62","#fc8bb9","#f3ee85","9fee60"), #https://jojo.fandom.com/f/p/4400000000000092610
   Yasuho = c("#fbedbe","#f87d99","#f394bf","#7dd7fb","#6b3ba7"), #https://www.color-hex.com/color-palette/38956
   VinegarDoppio = c("#f77199","#fcd1a1","#cf81d2","51ac7a","#68549d"), #https://www.color-hex.com/color-palette/91476
-  SmokeyBrown = c("#2b3d38","#ada247","#cac9c4","#55816f","#5f4439") #https://www.color-hex.com/color-palette/113255
+  SmokeyBrown = c("#2b3d38","#ada247","#cac9c4","#55816f","#5f4439"), #https://www.color-hex.com/color-palette/113255
+  Jolyne = c("#f272ca","#a2b711","#44b2b1","#0f6496","#141e39"), #https://jojo.fandom.com/f/p/4400000000000092610
+  Pucci = c("#402471","#e28f2f","#d4e0ed","#a45b33","#0086AD"),
+  FF = c("#7bb860","#508682","#e2d052","#d4d0c1","#c4452b")
 )
 
 jojo_stand <- function(name, n, type = c("discrete", "continuous")) {
